bdd69bcdb7710023f647af31fbd5a8f20a6b61ea
RianC4rl0s <riancarlos.sl@gmail.com>
2023-08-13 11:03:05 -0300

    feat: homepage

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:memorizer/level_1.dart';
-
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,15 +15,20 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0x433C57FF),
+      backgroundColor: Color(0xFF433C57),
       body: Form(
         key: _formKey,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(
-              "Memorizer",
+            const Icon(
+              FontAwesomeIcons.brain,
+              color: Colors.white,
+              size: 100.0,
+            ),
+            const Text(
+              "MEMORIZER",
               style: TextStyle(
                 fontSize: 32,
                 fontFamily: 'Lexend Mega',
@@ -31,65 +36,88 @@ class _HomePageState extends State<HomePage> {
                 color: Colors.white,
               ),
             ),
-            SizedBox(height: 40, width: 500,),
-            SizedBox(
-                width: 200,
-                height: 48,
-                child: ElevatedButton(
-                  onPressed: () async {
-                    late Widget page = Level1();
-                    String retorno = "";
-                    try {
-                      retorno = await push(context, page);
-                    } catch(error) {
-                      print(retorno);
-                    }
-                  },
-                  child: Text(
-                      'NEW GAME',
-                      style: TextStyle (
-                        fontSize: 20,
-                      fontFamily: 'Lexend Mega',
-                      )
-                  ),
-                ),
+            const SizedBox(
+              height: 75,
+              width: 500,
             ),
-            SizedBox(height: 20),
             SizedBox(
               width: 200,
               height: 48,
-              child: ElevatedButton(
-                  onPressed: () {
-                    // Respond to button press
-                  },
-                  child: Text(
-                      'ABOUT',
-                      style: TextStyle (
-                        fontSize: 20,
-                        fontFamily: 'Lexend Mega',
-                      )
-                  ),
+              child: OutlinedButton(
+                onPressed: () async {
+                  late Widget page = Level1();
+                  String retorno = "";
+                  try {
+                    retorno = await push(context, page);
+                  } catch (error) {
+                    print(retorno);
+                  }
+                },
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color(0xFF6F399A)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50.0),
+                      //side: const BorderSide(color: Color(0xFFAD00FF))
+                    ))),
+                child: const Text('NEW GAME',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontFamily: 'Lexend Mega',
+                      fontWeight: FontWeight.w500,
+                    )),
               ),
             ),
+            SizedBox(height: 10),
+            SizedBox(
+              width: 200,
+              height: 48,
+              child: OutlinedButton(
+                onPressed: () {
+                  // Respond to button press
+                },
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color(0xFF6F399A)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50.0),
+                      //side: const BorderSide(color: Color(0xFFAD00FF))
+                    ))),
+                child: const Text('ABOUT',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontFamily: 'Lexend Mega',
+                      fontWeight: FontWeight.w500,
+                    )),
+              ),
+            ),
+            const SizedBox(
+              width: 200,
+              height: 100,
+            )
           ],
         ),
       ),
     );
   }
 
-  Future push(BuildContext context, Widget page, {bool flagBack=true}) {
+  Future push(BuildContext context, Widget page, {bool flagBack = true}) {
     if (flagBack) {
       // Pode voltar, ou seja, a página é adicionada na pilha.
-      return Navigator.push(
-          context,
-          MaterialPageRoute(builder: (BuildContext context) { return page; })
-      );
+      return Navigator.push(context,
+          MaterialPageRoute(builder: (BuildContext context) {
+        return page;
+      }));
     } else {
       // Não pode voltar, ou seja, a página nova substitui a página atual.
-      return Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (BuildContext context) { return page; })
-      );
+      return Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (BuildContext context) {
+        return page;
+      }));
     }
   }
 }
