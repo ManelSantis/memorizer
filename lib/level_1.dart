548849ad662b9ae2739e1e3f82e3af1410689cfb
RianC4rl0s <riancarlos.sl@gmail.com>
2023-08-13 11:55:59 -0300

    refact: level1 page

--- lib/level_1.dart
+++ lib/level_1.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Level1 extends StatefulWidget {
   const Level1({super.key});
@@ -13,14 +15,103 @@ class _Level1State extends State<Level1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0x433C57FF),
+      backgroundColor: const Color(0xFF433C57),
+      appBar: AppBar(
+        toolbarHeight: 88,
+        centerTitle: true,
+        backgroundColor: const Color(0xFF2E2741),
+        title: const Column(
+          children: [
+            Icon(
+              FontAwesomeIcons.brain,
+              color: Colors.white,
+              size: 25.0,
+            ),
+            Text(
+              'MEMORIZE',
+              style: TextStyle(
+                fontFamily: 'LexendMega',
+                fontWeight: FontWeight.normal,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        enableFeedback: true,
+        elevation: 10,
+        hoverElevation: 50,
+        backgroundColor: const Color(0xFF41F393),
+        onPressed: () => {},
+        tooltip: 'Start Again',
+        child: const Icon(FontAwesomeIcons.arrowRotateLeft),
+      ),
       body: Form(
         key: _formKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child:  Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(
+            Container(
+              width: 323,
+              height: 73,
+              child: const Stack(
+                children: [
+                  Positioned(
+                    left: 87,
+                    top: 34,
+                    child: SizedBox(
+                      width: 145,
+                      height: 39,
+                      child: Text(
+                        '120 s',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontFamily: 'K2D',
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 0,
+                    top: 3,
+                    child: SizedBox(
+                      width: 56,
+                      height: 25,
+                      child: Text(
+                        'Lvl: 1',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontFamily: 'K2D',
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  ),
+                   Positioned(
+                    left: 217,
+                    top: 0,
+                    child: Text(
+                      'Score:100',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontFamily: 'K2D',
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Text(
               "Memorizer",
               style: TextStyle(
                 fontSize: 32,
@@ -29,7 +120,7 @@ class _Level1State extends State<Level1> {
                 color: Colors.white,
               ),
             ),
-            Text(
+            const Text(
               "Level 1",
               style: TextStyle(
                 fontSize: 32,
